408254c220e4c098e4fc0bc9f666fc0c91998336
nugmate <sixstep2002@gmail.com>
2021-07-30 22:35:54 +0300

    deleted redundant files, implemented linter and commands

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-    Content
-    </div>
-  );
+  return <div className="App">Content</div>;
 }
 
 export default App;
